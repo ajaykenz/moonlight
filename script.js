e9eeaf01495a08d77c7b63ef1943b14103905536
ajay <ajay@kenzsystems.com>
2025-01-13 09:53:39 +0530

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+const arrow = document.querySelector('#arr-img')
+const section2 = document.querySelector('#sec-2')
+
+
+const handleSlide = () => {
+    arrow.classList.add('animate__animated animate__bounceOutDown')
+    section2.style.transform="translateY(100)"
+}
+
+
+
+
